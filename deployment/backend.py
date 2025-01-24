77f1a4524efd3a64f6190f117fa2a670e2861c4a
Sunray0466 <son22092000@gmail.com>
2025-01-24 19:23:07 +0100

    Backend metrics

--- deployment/backend.py
+++ deployment/backend.py
@@ -5,8 +5,14 @@ from contextlib import asynccontextmanager
 import numpy as np
 import onnxruntime as rt
 from fastapi import FastAPI, File, HTTPException, UploadFile
+from prometheus_client import Counter, Histogram, Summary, make_asgi_app
 from PIL import Image
 
+# Error metrics
+error_counter = Counter("prediction_error", "Number of prediction errors")
+request_counter = Counter("prediction_requests", "Number of prediction requests")
+request_latency = Histogram("prediction_latency_seconds", "Prediction latency in seconds")
+review_summary = Summary("review_length_summary", "Review length summary")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -31,6 +37,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.mount("/metrics", make_asgi_app())
 
 
 @app.get("/")
@@ -46,12 +53,12 @@ def predict_image(img) -> list[str]:
 
     # get probabilities
     e_x = np.exp(output - np.max(output))
-    predicted_p = e_x.T / e_x.sum(axis=1)
+    predicted_p = (e_x.T / e_x.sum(axis=1))
     predicted_i = np.argsort(predicted_p, axis=0)[::-1][:3]
     predicted_c = predicted_i[0]
 
     prediction = []
-    prob = {}
+    prob   = {}
     for i in range(len(predicted_c)):
         prediction.append(idx2labels[predicted_c[i]])
         [print(j) for j in predicted_i[:, i]]
@@ -64,20 +71,20 @@ def predict_image(img) -> list[str]:
 @app.post("/classify/")
 async def classify_image(img_files: list[UploadFile] = list[File(...)]):
     """Classify image endpoint."""
-    # try:
-    img_arr = []
-    name_arr = []
-    for file in img_files:
-        byte_img = await file.read()
-        img = Image.open(io.BytesIO(byte_img)).resize((224, 224))
-        img = ((img - np.mean(img)) / np.std(img)).astype(np.float32)
-        img_arr.append(img)
-        name_arr.append(file.filename)
-    img_arr = np.asarray(img_arr)
-    img_arr = img_arr.transpose(0, 3, 1, 2)  # > batch,3,244,244
-    print("started prediction")
-    probabilities, prediction = predict_image(img_arr)
-    print("prop", probabilities, "pred", prediction)
-    return {"filename": name_arr, "prediction": prediction, "probabilities": probabilities}
-    # except Exception as e:
-    #     raise HTTPException(status_code=500) from e
+    request_counter.inc()
+    try:
+        img_arr = []
+        name_arr = []
+        for file in img_files:
+            byte_img = await file.read()
+            img = Image.open(io.BytesIO(byte_img)).resize((224, 224))
+            img = ((img - np.mean(img)) / np.std(img)).astype(np.float32)
+            img_arr.append(img)
+            name_arr.append(file.filename)
+        img_arr = np.asarray(img_arr)
+        img_arr = img_arr.transpose(0, 3, 1, 2)  # > batch,3,244,244
+        probabilities, prediction = predict_image(img_arr)
+        return {"filename": name_arr, "prediction": prediction, "probabilities": probabilities}
+    except Exception as e:
+        error_counter.inc()
+        raise HTTPException(status_code=500, detail=str(e)) from e
